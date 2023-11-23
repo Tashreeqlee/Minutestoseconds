8010ba682d47f698921af56acb85e385617db6d0
Tashreeqlee <tashyct14@gmail.com>
2023-11-23 12:43:55 +0200

    Javascript code

--- script/code.js
+++ script/code.js
@@ -1,1 +1,9 @@
-//
+const minutesInput = document.getElementById('minutesInput');
+        const convertButton = document.getElementById('convertButton');
+        const result = document.getElementById('result');
+
+        convertButton.addEventListener('click', () => {
+            const minutes = parseInt(minutesInput.value);
+            const seconds = minutes * 60;
+            result.textContent = `${minutes} minutes is equal to ${seconds} seconds.`;
+        });
